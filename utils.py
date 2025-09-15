77f00863566407fe95454da07d8dc7319c34e72c
kazi <87664487+abirhosen069@users.noreply.github.com>
2025-09-15 20:39:50 +0600

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+# utils.py
+import logging
+
+
+def setup_logger(name=__name__):
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+return logging.getLogger(name)
